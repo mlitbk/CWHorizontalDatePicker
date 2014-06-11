239b98ee478ec622c82cbc1d8096828e964db82d
Michael Litvak <michael@codewhisper.com>
2014-06-11 12:05:03 +0300

    Initial Commit

---
+++ CWDatePicker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CWDatePicker
+//
+//  Created by Michael Litvak on 6/11/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
